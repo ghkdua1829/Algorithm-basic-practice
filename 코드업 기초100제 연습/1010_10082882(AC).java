8e15b69802f63df2ccfa3af2bbe521119aca43fc
ghkdua1829 <ghkdua1829@naver.com>
2019-08-10 21:08:37 +0900

    1~11 번 풀이

---
+++ 코드업 기초100제 연습/1010_10082882(AC).java
@@ -0,0 +1,11 @@
+﻿import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int a=scanner.nextInt();
+		System.out.print(a);
+	}
+
+}
+
---
+++ 코드업 기초100제 연습/1011_10082889(AC).java
@@ -0,0 +1,11 @@
+﻿import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		String a=scanner.nextLine();
+		System.out.print(a);
+	}
+
+}
+
